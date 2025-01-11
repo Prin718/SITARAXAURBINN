c5b2c79512a46debc98f14d6e539cb3b23dff19c
Patel070707 <princek567098@gmail.com>
2025-01-12 00:32:46 +0530

    Update play.py

--- MoonXMusic/utils/inline/play.py
+++ MoonXMusic/utils/inline/play.py
@@ -59,19 +59,14 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-    [ 
-    InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
+        ], 
             )
-        ],
-      [
+         [
             InlineKeyboardButton(
                 text="• ᴜᴘᴅᴀᴛᴇ •", url ="https://t.me/ll_BOTCHAMBER_ll",
             ),
             InlineKeyboardButton(
-                text="• sᴜᴘᴘᴏʀᴛ •", url="https://t.me/BHAICHARAONTOP8338",
+                text="• sᴜᴘᴘᴏʀᴛ •", url="https://t.me/BHAICHARAONTOP78",
             )
         ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -93,7 +88,7 @@ def stream_markup(_, chat_id):
                 text="• ᴜᴘᴅᴀᴛᴇ •", url="https://t.me/ll_BOTCHAMBER_ll",
             ),
             InlineKeyboardButton(
-                text="• sᴜᴘᴘᴏʀᴛ •", url="https://t.me/BHAICHARAONTOP8338",
+                text="• sᴜᴘᴘᴏʀᴛ •", url="https://t.me/BHAICHARAONTOP78",
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
